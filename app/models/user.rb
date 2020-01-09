9a57ad3f1781a9dcdb8174c965475f59c91eb38b
rOluochKe <rasholuoch@gmail.com>
2020-01-10 00:25:00 +0300

    login session logout

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,27 @@ class User < ApplicationRecord
   validates :email, presence: true, length: { maximum: 255 },
                     format: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
+  before_create :create_remember_token
   has_secure_password
   validates :password, presence: true, length: { minimum: 6 }
+
+  # Returns a random token.
+  def self.new_remember_token
+    SecureRandom.urlsafe_base64
+  end
+
+  def self.digest(token)
+    Digest::SHA1.hexdigest(token.to_s)
+  end
+
+  # Forgets a user.
+  def forget
+    update_attribute(:remember_digest, nil)
+  end
+
+  private
+  def create_remember_token
+    self.remember_token = User.digest(User.new_remember_token)
+  end
 
 end
