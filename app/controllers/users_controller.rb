b00e61ed45865e19b1b2368417907ec69e2cd609
rOluochKe <rasholuoch@gmail.com>
2020-01-11 12:38:47 +0300

    linting

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -29,10 +29,10 @@ class UsersController < ApplicationController
 
   # Confirms a logged-in user.
   def logged_in_user
-    unless logged_in?
-      store_location
-      flash[:danger] = 'Please log in.'
-      redirect_to @user
-    end
+    return unless logged_in?
+
+    store_location
+    flash[:danger] = 'Please log in.'
+    redirect_to @user
   end
 end
